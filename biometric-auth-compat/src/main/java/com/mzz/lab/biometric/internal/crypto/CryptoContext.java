6e0e4f1b8396f0fd5ffcc143d8fa3c6614643f6b
Enrico <enrico.mazzucchelli@vipera.com>
2019-05-29 21:59:16 +0200

    Step 1) define new api layer

---
+++ biometric-auth-compat/src/main/java/com/mzz/lab/biometric/internal/crypto/CryptoContext.java
@@ -0,0 +1,103 @@
+package com.mzz.lab.biometric.internal.crypto;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.security.keystore.KeyGenParameterSpec;
+import android.security.keystore.KeyPermanentlyInvalidatedException;
+import android.security.keystore.KeyProperties;
+
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+
+public class CryptoContext {
+
+    private Cipher cipher;
+    private KeyStore keyStore;
+    private KeyGenerator keyGenerator;
+
+
+    public CryptoContext(String keyName) throws CryptoContextInitException {
+        try {
+            generateKey(keyName);
+            initCipher(keyName);
+        }catch (RuntimeException ex){
+            throw new CryptoContextInitException(ex.getCause());
+        }
+
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void generateKey(String keyName) {
+        try {
+
+            keyStore = KeyStore.getInstance("AndroidKeyStore");
+            keyStore.load(null);
+
+            keyGenerator = KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore");
+            keyGenerator.init(new
+                    KeyGenParameterSpec.Builder(keyName, KeyProperties.PURPOSE_ENCRYPT | KeyProperties.PURPOSE_DECRYPT)
+                    .setBlockModes(KeyProperties.BLOCK_MODE_CBC)
+                    .setUserAuthenticationRequired(true)
+                    .setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_PKCS7)
+                    .build());
+
+            keyGenerator.generateKey();
+
+        } catch (KeyStoreException
+                | NoSuchAlgorithmException
+                | NoSuchProviderException
+                | InvalidAlgorithmParameterException
+                | CertificateException
+                | IOException exc) {
+           throw new RuntimeException(exc);
+        }
+    }
+
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private boolean initCipher(String keyName) {
+        try {
+            cipher = Cipher.getInstance(
+                    KeyProperties.KEY_ALGORITHM_AES + "/"
+                            + KeyProperties.BLOCK_MODE_CBC + "/"
+                            + KeyProperties.ENCRYPTION_PADDING_PKCS7);
+
+        } catch (NoSuchAlgorithmException |
+                NoSuchPaddingException e) {
+            throw new RuntimeException("Failed to get Cipher", e);
+        }
+
+        try {
+            keyStore.load(null);
+            SecretKey key = (SecretKey) keyStore.getKey(keyName,
+                    null);
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+            return true;
+
+
+        } catch (KeyPermanentlyInvalidatedException e) {
+            return false;
+
+        } catch (KeyStoreException | CertificateException
+                | UnrecoverableKeyException | IOException
+                | NoSuchAlgorithmException | InvalidKeyException e) {
+
+            throw new RuntimeException("Failed to init Cipher", e);
+        }
+    }
+}
---
+++ biometric-auth-compat/src/main/java/com/mzz/lab/biometric/internal/crypto/CryptoContextInitException.java
@@ -0,0 +1,8 @@
+package com.mzz.lab.biometric.internal.crypto;
+
+public class CryptoContextInitException extends Exception {
+
+    public CryptoContextInitException(Throwable cause) {
+        super(cause);
+    }
+}
